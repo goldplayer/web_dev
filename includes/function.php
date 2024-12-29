40a44b8adff699a6ac413fda50fcae7f7ec41ccc
kamayoti <kostya.karaman44@gmail.com>
2024-12-29 20:01:00 +0100

    f

--- includes/function.php
+++ includes/function.php
@@ -1,4 +1,10 @@
 <?php
 
+    require_once ('db_connect.php');
 
+    $q = $pdo->prepare('select * from `admin`');
+    $q -> execute();
+    $rows = $q->fetchAll(PDO::FETCH_ASSOC);
 
+    var_dump($q);
+    print_r($rows);
