2b50a42cfa0d1ffc13bbe3b41475d1e00c2246fc
kamacoder <kamacoder.work@gmail.com>
2024-12-30 00:25:25 +0100

    f

--- includes/function.php
+++ includes/function.php
@@ -2,9 +2,21 @@
 
     require_once ('db_connect.php');
 
+    function get_admin(){
+    global $pdo;
     $q = $pdo->prepare('select * from `admin`');
     $q -> execute();
     $rows = $q->fetchAll(PDO::FETCH_ASSOC);
 
-    var_dump($q);
-    print_r($rows);
+    if($rows){
+        echo 'ok';
+    }else{
+        return false;
+    }
+
+
+
+    }
+
+
+  
--- view/admin.php
+++ view/admin.php
@@ -1,3 +1,11 @@
+<?php
+    include 'includes/function.php';
 
+    if(get_admin()){
+        // header('Location: /web-dev/admin');
+    }
+
+
+?>
 
 <main>Hello Admin</main>
