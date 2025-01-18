d244299474c970d1636c7b6c073ce2677a4b7ade
kamacoder <kamacoder.work@gmail.com>
2025-01-18 20:48:14 +0100

    f

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,20 @@
 
-<h2>footer</h2>
+
     
+<div class="container footer">
+    <div class="row">
+        <div class="col-6 col-md-6 col-lg-6">
+            <a class="logo" href="index">D_Photo</a>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Error voluptatibus tempora aliquid qui dolor culpa quod, quibusdam, vero eos dolorem ducimus unde sapiente iste inventore dolorum consectetur quidem incidunt totam!
+            </p>
+        </div>
+        <div class="col-6 col-md-6 col-lg-6"></div>
+    </div>
+</div>
+
+
 </body>
 </html>
 
