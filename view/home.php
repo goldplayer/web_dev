1f3e90faf1ea18e03c32b9900ae84b6bcdc2c7e6
kamacoder <kamacoder.work@gmail.com>
2025-01-20 22:50:40 +0100

    f

--- view/home.php
+++ view/home.php
@@ -1,5 +1,23 @@
+<!-- home page -->
 
-<main class="container">
-  <a href="./admin">Admin</a>
-</main>
-
+<section class="first_block">
+  <div class="container">
+    <div class="row">
+      <div class="col-5 col-md-5 col-lg-5">
+        <div class="first_block_image">
+          <img src="./assets/images/test.png" alt="" />
+        </div>
+      </div>
+      <div class="col-7 col-md-7 col-lg-7">
+        <div class="first_block_information">
+          <h1 class="first_block_heading_h1">
+            Photography isn’t just about taking pictures; it’s about telling
+            your story. Let’s create something extraordinary together, one shot
+            at a time.
+          </h1>
+        </div>
+        <div class="row justify-content-end"></div>
+      </div>
+    </div>
+  </div>
+</section>
