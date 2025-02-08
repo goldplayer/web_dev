e68bfd8e1c2aaad0f151596f205ded95c2d1f6c3
kamayoti <kostya.karaman44@gmail.com>
2025-02-08 18:19:51 +0100

    f

--- view/home.php
+++ view/home.php
@@ -1,36 +1,14 @@
 <!-- home page -->
 
-<section class="first_block">
+<div class="first_section">
   <div class="container">
-    <div class="row">
-      <div class="col-5 col-md-5 col-lg-5">
-        <div class="first_block_image">
-          <img src="./assets/images/test.png" alt="" />
-        </div>
-      </div>
-      <div class="col-7 col-md-7 col-lg-7">
-        <div class="first_block_information">
-          <h1 class="first_block_heading_h1">
-            Photography isn’t just about taking pictures; it’s about telling
-            your story. Let’s create something extraordinary together, one shot -
-            one kill.
-          </h1>
-            <div class="btn_descr">
-                <p class="btn_descr">Lorem ipsum dolor sit amet, consecte adipiscin elit, sed do eiusmod tempor incididunt labore et dolore magna aliqua. Ut enim minim</p>
-                <button class="get_started animate__animated">Get Started</button>
-            </div>
-
-            <div class="first_block_bottom_content">
-                <div class="col-3 col-md-6 col-lg-6">
-                    <span class="header_line"></span>
-                    <h3>Passion Meets Precision Behind Every Shot.</h3>
-                </div>
-                <div class="col-3 col-md-6 col-lg-6">
-                    <img src="./assets/images/test_2.png" alt="">
-                </div>
-            </div>
-        </div>
-      </div>
-    </div>
+    
+  <div class="left_column">
+      <img src="././assets/images/test.png" alt="">
   </div>
-</section>
+
+  <div class="right_column">
+    <h1>Photography isn’t just about taking pictures; it’s about telling your story. Let’s create something extraordinary together, one shot at a time.</h1>
+  </div>
+  </div>
+</div>
