704247c909557813099944e153d840ac13d00e76
kamayoti <kostya.karaman44@gmail.com>
2025-02-08 23:18:50 +0100

    f

--- includes/header.php
+++ includes/header.php
@@ -5,20 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
     <link rel="stylesheet" href="./assets/css/main.css">
+    <link rel="stylesheet" href="./assets/css/media_query.css" media="screen and (max-width:1680px)">
   </head>
   <body>
     <header>
       <div class="container">
         <div class="row">
-          <div class="col-lg-6 col-md-6">
+          <div class="header_menu">
           <div class="logo_header">
               <a href="home">D_Photo</a>
           </div>
-          </div>
-          <div class="col-lg-6 col-md-6">
-                    <!-- burger -->
+                       <!-- burger -->
          <button class="burger" id="burger">
           <span></span>
           <span></span>
@@ -35,6 +34,24 @@
           </nav>
          </div>
           </div>
+          <!-- <div class="col-lg-6 col-md-6">
+                    
+         <button class="burger" id="burger">
+          <span></span>
+          <span></span>
+          <span></span>
+         </button>
+
+         <div class="menu-overlay" id="menuOverlay">
+          <nav class="menu">
+            <ul>
+              <li><a href="home">Home</a></li>
+              <li><a href="gallery">Gallery</a></li>
+              <li><a href="admin">Admin</a></li>
+            </ul>
+          </nav>
+         </div>
+          </div> -->
         </div>
       </div>
     </header>
