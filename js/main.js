efa908b5374a3ae8266ea1791a49813bb5479d69
kamayoti <kostya.karaman44@gmail.com>
2025-03-25 17:44:22 +0100

    f

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
 AOS.init();
+
+document.addEventListener("DOMContentLoaded", function() {
+    const burgerButton = document.querySelector(".burger-button");
+    const fullscreenMenu = document.getElementById("fullscreenMenu");
+    const closeMenuBtn = document.getElementById("closeMenuBtn");
+
+    burgerButton.addEventListener("click", () => {
+        fullscreenMenu.classList.toggle("active");
+    });
+
+    closeMenuBtn.addEventListener("click", () => {
+        fullscreenMenu.classList.remove("active");
+    });
+});
